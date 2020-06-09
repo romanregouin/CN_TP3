8187e386fa43fc1f52905590be6eb042820d2e07
romanregouin <romanregouin@live.fr>
2020-06-09 18:21:54 +0200

    separation of files

--- decodeurViterbiNoDigital.c
+++ decodeurViterbiNoDigital.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
+#include "generator.h"
 #include "math.h"
 
 typedef struct n
@@ -138,22 +137,6 @@ noeud *nouveauNoeud(noeud *precedent, float *code, int indice, int etat,
     }
     new->suivant1 = NULL;
     return new;
-}
-
-double randomizer(double a, double b)
-{
-    return (rand() / (double)RAND_MAX) * (b - a) + a;
-}
-
-// suppose que la graine a ete initialise
-double *getWord(double a, double b, int len)
-{
-    double *word = malloc(len * sizeof(double));
-    for (int i = 0; i < len; i++)
-    {
-        word[i] = (rand() / (double)RAND_MAX) * (b - a) + a;
-    }
-    return word;
 }
 
 void decoder(noeud *n, int *m, int taille)
@@ -293,100 +276,6 @@ int *decodeur(float *code, int taille)
     decoder(tete, message, taille / 2);
 
     return message;
-}
-
-//bin = 1 si le mot est en binaire
-void printWord(double *word, int *wordBin, int len, int bin)
-{
-    for (int i = 0; i < len; i++)
-    {
-        if (bin)
-        {
-            printf("%d ", wordBin[i]);
-        }
-        else
-        {
-            printf("%f ", word[i]);
-        }
-    }
-    printf("\n");
-}
-
-// suppose que le descripteur est deja ouvert
-void ecrireWord(double *word, int len, FILE *f)
-{
-    fprintf(f, "%d\n", len);
-    for (int i = 0; i < len; i++)
-    {
-        fprintf(f, "%f ", word[i]);
-    }
-    fprintf(f, "\n");
-}
-
-//code : reel <= 0.5 -> 0 / > 0.5 -> 1 , introduit une erreur si reel = 0.5 [+-0.1]
-int *toBinariesWithErrors(double *word, int len)
-{
-    int *result = malloc(len * sizeof(int));
-    for (int i = 0; i < len; i++)
-    {
-        double tmp = word[i];
-        if (tmp <= 0.5)
-        {
-            //introduction d'erreur
-            if (tmp + 0.1 >= 0.5)
-            {
-                result[i] = 1;
-            }
-            else
-            {
-                result[i] = 0;
-            }
-        }
-        else
-        {
-            //introduction d'erreur
-            if (tmp - 0.1 <= 0.5)
-            {
-                result[i] = 0;
-            }
-            else
-            {
-                result[i] = 1;
-            }
-        }
-    }
-    return result;
-}
-
-//stocke les mots dans un fichier result.txt si il nexiste pas le cree
-int mainV2(int argc, char const *argv[])
-{
-    if (argc != 3)
-    {
-        printf("Syntaxe : %s [lenWord1] [lenWord2]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    int len1 = atoi(argv[1]);
-    int len2 = atoi(argv[2]);
-    FILE *resultFile = fopen("result.txt", "w");
-    if (resultFile == NULL)
-    {
-        printf("Erreur overture fichier\n");
-        return EXIT_FAILURE;
-    }
-    srand((unsigned)time(NULL));
-    double *word1 = getWord(0.0, 1.0, len1);
-    double *word2 = getWord(0.0, 1.0, len2);
-    printWord(word1, NULL, len1, 0);
-    printWord(word2, NULL, len2, 0);
-    ecrireWord(word1, len1, resultFile);
-    ecrireWord(word2, len2, resultFile);
-    fclose(resultFile);
-    int *word1Binaries = toBinariesWithErrors(word1, len1);
-    int *word2Binaries = toBinariesWithErrors(word2, len2);
-    printWord(NULL, word1Binaries, len1, 1);
-    printWord(NULL, word2Binaries, len2, 1);
-    return 0;
 }
 
 int main(int argc, char const *argv[])
