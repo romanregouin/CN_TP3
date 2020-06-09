533c5dd7480ff7dd655f49ddf56e25644ee3e727
romanregouin <romanregouin@live.fr>
2020-06-09 17:35:07 +0200

    add possibility to choose width of word

--- decodeurViterbiNoDigital.c
+++ decodeurViterbiNoDigital.c
@@ -239,21 +239,24 @@ void ecrireWord(double* word, int len, FILE* f){
 
 //stocke les mots dans un fichier result.txt si il nexiste pas le cree
 int main(int argc, char const* argv[]) {
-    if(argc!=1){
-        printf("Syntaxe : ./%s",argv[0]);
+    if(argc!=3){
+        printf("Syntaxe : %s [lenWord1] [lenWord2]\n",argv[0]);
+        return EXIT_FAILURE;
     }
+    int len1 = atoi(argv[1]);
+    int len2 = atoi(argv[2]);
     FILE* resultFile = fopen("result.txt","w");
     if(resultFile==NULL){
         printf("Erreur overture fichier\n");
         return EXIT_FAILURE;
     }
     srand((unsigned) time(NULL));
-    double* word1 = getWord(0.0,1.0,4); 
-    double* word2 = getWord(0.0,1.0,6);
-    printWord(word1,4);
-    printWord(word2,6);
-    ecrireWord(word1,4,resultFile);
-    ecrireWord(word2,6,resultFile);
+    double* word1 = getWord(0.0,1.0,len1); 
+    double* word2 = getWord(0.0,1.0,len2);
+    printWord(word1,len1);
+    printWord(word2,len2);
+    ecrireWord(word1,len1,resultFile);
+    ecrireWord(word2,len2,resultFile);
     fclose(resultFile);
     return 0;
 }
